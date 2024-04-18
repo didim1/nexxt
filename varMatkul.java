d939615f9dff1fe951827f5708c6fb7614d56acf
didim.solos <96524806+didim1@users.noreply.github.com>
2024-04-18 14:38:54 +0700

    Add files via upload

---
+++ varMatkul.java
@@ -0,0 +1,48 @@
+
+package Model;
+
+/**
+ *
+ * @author Acer
+ */
+public class varMatkul {
+    public String vKodematkul;
+    public String vNama;
+    public String vSKS;
+    public String vKdpersyarat;
+
+ 
+    public String getvKodematkul() {
+        return vKodematkul;
+    }
+
+    public void setvKodematkul(String vKodematkul) {
+        this.vKodematkul = vKodematkul;
+    }
+
+    public String getvNama() {
+        return vNama;
+    }
+
+    public void setvNama(String vNama) {
+        this.vNama = vNama;
+    }
+
+    public String getvSKS() {
+        return vSKS;
+    }
+
+    public void setvSKS(String vSKS) {
+        this.vSKS = vSKS;
+    }
+
+    public String getvKdpersyarat() {
+        return vKdpersyarat;
+    }
+
+    public void setvKdpersyarat(String vKdpersyarat) {
+        this.vKdpersyarat = vKdpersyarat;
+    }
+    
+    
+}
