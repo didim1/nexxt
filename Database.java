eac83febe4b92ebc62fff5e8251a486c69e222b5
didim.solos <96524806+didim1@users.noreply.github.com>
2024-04-18 14:38:34 +0700

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,33 @@
+package Koneksi;
+import java.util.Properties;
+import java.sql.Connection;
+import java.io.FileInputStream;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+
+public class Database {
+    static  Properties  mypanel;
+    static String      driver, database,user,pass; 
+    static Connection conn; 
+
+    public static Connection KoneksiDB(){
+        if(conn == null){
+            try {
+                mypanel = new Properties();
+                mypanel.load(new FileInputStream("lib/database.ini"));
+                driver    = mypanel.getProperty("DBDriver1");
+                database  =  mypanel.getProperty("DBDatabase1");
+                user  =  mypanel.getProperty("DBUsername");
+                pass  =  mypanel.getProperty("DBPassword");
+            
+                Class.forName(driver).newInstance();
+                conn = DriverManager.getConnection(database,user,pass);                
+                JOptionPane.showMessageDialog(null,"Koneksi Berhasil!","Pesan",JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception ex) {                
+                JOptionPane.showMessageDialog(null,"Koneksi Tidak Berhasil!","Pesan",JOptionPane.INFORMATION_MESSAGE);
+                System.out.println("Error : "+ex.getMessage());
+            } 
+        }
+        return conn;
+    }    
+}
